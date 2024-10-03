8830281f8d1a30e99fcee49e3071820bdb32dd2e
James Tsetsekas <Jtsetsekas@gmail.com>
2024-10-03 09:12:34 -0400

    Change LN Address ALBY

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,4 +1,4 @@
-export const LIGHTNING_ADDRESS = 'James@btc.JamesTsetsekas.com';
+export const LIGHTNING_ADDRESS = 'jamestsetsekas@getalby.com';
 
 // export const MILLISATS_PER_MESSAGE = 200_000;
 export const MILLISATS_PER_MESSAGE = 30_000;
